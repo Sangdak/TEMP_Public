d26f71465647c714e94bee573c574ef29c551368
Sangdak <sh.doppelganger@gmail.com>
2022-07-14 00:21:31 +1000

    Rename few variables to explain its existence.

--- course_api/lsn_2__bitly/main.py
+++ course_api/lsn_2__bitly/main.py
@@ -4,49 +4,46 @@ from urllib.parse import urlparse
 import requests
 
 
-def is_bitlink(url, token):
-    tool_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}'.format(url)
-    print(f'is bitlink: {tool_url}')
+def is_bitlink(url, header):
+    request_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}'.format(url)
     try:
-        response = requests.get(tool_url, headers=token)
+        response = requests.get(request_url, headers=header)
         response.raise_for_status()
     except:
         return False
     return True
 
 
-def shorten_url(url, token):
-    tool_url = 'https://api-ssl.bitly.com/v4/bitlinks'
+def shorten_url(url, header):
+    request_url = 'https://api-ssl.bitly.com/v4/bitlinks'
     payload = {"long_url": "{}".format(url)}
-    print(f'shorten: {tool_url}')
 
-    response = requests.post(tool_url, headers=token, json=payload)
+    response = requests.post(request_url, headers=header, json=payload)
     response.raise_for_status()
     return response.json()["id"]
 
 
-def count_clicks(url, token):
-    tool_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(url)
-    print(f'count: {tool_url}')
+def count_clicks(url, header):
+    request_url = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(url)
 
-    response = requests.get(tool_url, headers=token)
+    response = requests.get(request_url, headers=header)
     response.raise_for_status()
     return response.json()["total_clicks"]
 
 
 def main():
-    token = {"Authorization": "Bearer {}".format(os.getenv("TOKEN"))}
+    autorization_data = {"Authorization": "Bearer {}".format(os.getenv("TOKEN"))}
     input_url = input('Input your URL: ')
     parse = urlparse(input_url)
-    formated_url = parse.netloc + parse.path
-    url_is_bitlink = is_bitlink(formated_url, token)
+    formatted_url = parse.netloc + parse.path
+    url_is_bitlink = is_bitlink(formatted_url, autorization_data)
 
     try:
         if not url_is_bitlink:
-            bitlink = shorten_url(input_url, token)
+            bitlink = shorten_url(input_url, autorization_data)
             return f'Битлинк {bitlink}'
         else:
-            clicks = count_clicks(formated_url, token)
+            clicks = count_clicks(formatted_url, autorization_data)
             return f'По вашей ссылке перешли {clicks} раз(а).'
     except requests.exceptions.HTTPError:
         return f'Сссылка {input_url} не может быть обработана, проверьте ввод!'
